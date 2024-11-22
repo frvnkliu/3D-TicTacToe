3e19c070e484e2a937e7c8ce55ed74997531ce87
frvnk <frvnkliu@gmail.com>
2024-11-22 13:39:10 -0500

    Added point hovering detection and indication

--- script.js
+++ script.js
@@ -29,19 +29,51 @@ cubeFamily.add(axesHelper);
 const gridHelper = new THREE.GridHelper(cubeLength*2, 12);
 cubeFamily.add(gridHelper);
 */
+const g = new THREE.SphereGeometry(1.2, 32, 16); 
+const m = new THREE.MeshBasicMaterial( { color: 0x82EEFD, depthTest: false} ); 
 
-// Making points for each cube
-for (let i=-(cubeLength/2); i<=(cubeLength/2);i+=(cubeLength/3)){
-  for (let j=-(cubeLength/2); j<=(cubeLength/2);j+=(cubeLength/3)){
-    for (let k=-(cubeLength/2); k<=(cubeLength/2);k+=(cubeLength/3)){
-      const g = new THREE.SphereGeometry(1, 32, 16); 
-      const m = new THREE.MeshBasicMaterial( { color: 0xE1F0FA, transparent: true, opacity: 0.5, depthTest: false} ); 
-      const sphere = new THREE.Mesh( g, m );
-      sphere.position.set(i, j, k);
+// Making spheres points for each point on 4x4 grid
+const points = new Array(4).fill(0).map( () =>
+  new Array(4).fill(0).map( () =>
+    new Array(4).fill(0).map(()=>
+      new THREE.Mesh( g, m )
+    )
+  )
+);
+
+for (let i= 0; i< 4; i++){
+  const x = -cubeLength/2 + i *(cubeLength/3);
+  for (let j= 0; j< 4; j++){
+    const y = -cubeLength/2 + j *(cubeLength/3);
+    for (let k= 0; k< 4; k++){
+      const z = -cubeLength/2 + k *(cubeLength/3);
+      const sphere = points[i][j][k];
+      console.log(x, y, z);
+      sphere.position.set(x, y, z);
+      sphere.coord = [i,j,k];
+      sphere.visible = false;
       cubeFamily.add( sphere );
     }
   }
 }
+
+const g2 = new THREE.SphereGeometry(1, 32, 16); 
+const m2 = new THREE.MeshBasicMaterial( { color: 0xf0f8ff, transparent: true, opacity: 0.5, depthTest: false} ); 
+for (let i= 0; i< 4; i++){
+  const x = -cubeLength/2 + i *(cubeLength/3);
+  for (let j= 0; j< 4; j++){
+    const y = -cubeLength/2 + j *(cubeLength/3);
+    for (let k= 0; k< 4; k++){
+      const z = -cubeLength/2 + k *(cubeLength/3);
+      const sphere = new THREE.Mesh(g2,m2);
+      sphere.position.set(x, y, z);
+      cubeFamily.add( sphere );
+    }
+  }
+}
+
+const flatPoints = points.flat(2);
+//console.log(flatPoints);
 
 /*
   Creating "wireframe" lines inbetween our points
@@ -89,17 +121,10 @@ const axisX = new THREE.Vector3(1, 0, 0);
 const axisY = new THREE.Vector3(0, 1, 0);
 var raycaster = new THREE.Raycaster();
 
-function isHover(X, Y){
-    //check if mouse is over sphere
-    var mouseVector = new THREE.Vector2(X, Y);
-    raycaster.setFromCamera(mouseVector, camera);
-    var intersects = raycaster.intersectObject(wireframeCube);
-    return intersects.length>0;
-}
-
 // Variables for tracking mouse movement
-var hovered = false;
+var hovered = false; // checks if the cube is Hovered
 let isDragging = false;
+
 let previousMousePosition = {
   x: 0,
   y: 0
@@ -115,30 +140,64 @@ document.addEventListener('mouseup', (event) => {
   isDragging = false;
 });
 
+
+function isHover(X, Y){
+  //check if mouse is over sphere
+  var mouseVector = new THREE.Vector2(X, Y);
+  raycaster.setFromCamera(mouseVector, camera);
+  var intersects = raycaster.intersectObject(wireframeCube);
+  return intersects.length>0;
+}
+
+function firstHovered(X, Y){
+  var mouseVector = new THREE.Vector2(X, Y);
+  raycaster.setFromCamera(mouseVector, camera);
+  var intersectObjects = raycaster.intersectObjects(flatPoints, false);
+  return intersectObjects[0]?.object;
+}
+
+const labelDiv = document.getElementById('label');
+
+let prevPoint = null;
 // Mouse move event to rotate the cube based on mouse movement
 document.addEventListener('mousemove', (event) => {
     var X = (event.clientX / window.innerWidth) * 2 - 1;
     var Y = -(event.clientY / window.innerHeight) * 2 + 1;
-    //find if element is hovered
 
-    if (isDragging) {
-        // Calculate the change in mouse position
-        const deltaMove = {
+
+    if(isDragging){
+      // Calculate the change in mouse position
+      const deltaMove = {
         x: event.clientX - previousMousePosition.x,
         y: event.clientY - previousMousePosition.y
-        };
+      };
 
-        // Update cube rotation based on mouse movement
-        cubeFamily.rotateOnWorldAxis(axisY, deltaMove.x * 0.007);
-        cubeFamily.rotateOnWorldAxis(axisX, deltaMove.y * 0.007);
+      // Update cube rotation based on mouse movement
+      cubeFamily.rotateOnWorldAxis(axisY, deltaMove.x * 0.007);
+      cubeFamily.rotateOnWorldAxis(axisX, deltaMove.y * 0.007);
     }else{
+      //1. Find if element is hovered
+      const point = firstHovered(X,Y);
+      if(point){
+        //console.log(point.coord);
+        if(prevPoint) prevPoint.visible = false;
+        point.visible = true;
+        labelDiv.innerText = `Point Selected: (${point.coord})`;
+        container.classList.remove('hovered');
+        container.classList.add('select');
+        isDragging = false;
+        hovered=false;
+        prevPoint = point;
+      } else{
+        prevPoint.visible = false;
+        container.classList.remove('select');
         hovered = isHover(X, Y);
-        console.log(hovered);
         if (hovered) {
-            container.classList.add('hovered');
+          container.classList.add('hovered');
         }else{
             container.classList.remove('hovered');
         }
+      }
     }
 
     // Update the previous mouse position to the current position
@@ -165,10 +224,10 @@ animate();
 
 function onWindowResize() {
     // Update the size of the renderer
-    renderer.setSize(window.innerWidth, window.innerHeight*.95);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
     // Update the aspect ratio of the camera
-    camera.aspect = window.innerWidth / (window.innerHeight*.95);
+    camera.aspect = window.innerWidth / (window.innerHeight);
     camera.updateProjectionMatrix();
 }
 // Add event listener for window resize
