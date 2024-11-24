f51781bf6c3d70bec81561118db5308d1a4adab4
frvnk <frvnkliu@gmail.com>
2024-11-24 13:18:38 -0500

    added more comments to skeleton

--- TicTacToe3D.js
+++ TicTacToe3D.js
@@ -1,10 +1,11 @@
 export class TicTacToe3D{
-
     /*
         Constructor for our game simulator
     */
     constructor(){
-
+        //ToDo: Initialize 4x4x4 board
+        this.board = []
+        //ToDo?: Initialize column/row/diagonal sums for checking if player has won
     }
     
     /*
